16412c211383ff0fd0fe6bd7ec485ef70a99d97b
suyuan <suyuan326@qq.com>
2019-07-22 00:08:08 +0800

    use scoped and scss

--- src/views/Register/template.vue
+++ src/views/Register/template.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="register">
     <h1>注册页面</h1>
+    <p>注册注册</p>
   </div>
 </template>
 <script src="./template.js"></script>
--- src/views/User/template.vue
+++ src/views/User/template.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="user">
     <h1>用户页面</h1>
+    <p>haha</p>
   </div>
 </template>
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+<style scoped lang="scss" src="./template.scss"></style>
 
