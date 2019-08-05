a0dbd4bc9d137c8715623854d83b92e505a8dfc0
suyuan <suyuan326@qq.com>
2019-08-05 23:20:57 +0800

    add my page

--- src/views/My/template.vue
+++ src/views/My/template.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="my">
-    <h1>我的页面</h1>
+  <div id="my">
+    <section class="user-info">
+      <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
+      <h3>用户名</h3>
+    </section>
+    <section>
+      <div class="item">
+        <div class="date">
+          <span class="day">5</span>
+          <span class="month">8月</span>
+          <span class="year">2019</span>
+        </div>
+        <h3>标题</h3>
+        <p>内容CSS 网格布局(Grid Layout) 是CSS中最强大的布局系统。 这是一个二维系统，这意味着它可以同时处理列和行，不像CSS 网格布局(Grid Layout) 是CSS中最强大的布局系统。 这是一个二维系统，这意味着它可以同时处理列和行，不像CSS 网格布局(Grid Layout) 是CSS中最强大的布局系统。 这是一个二维系统，这意味着它可以同时处理列和行，不像</p>
+        <div class="actions">
+          <router-link to="edit">编辑 </router-link>
+          <a href="#">删除</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+<style lang="scss" src="./template.scss"></style>
 
