3c709c17c63e528365a439f35d00053d471d6ad0
suyuan <suyuan326@qq.com>
2019-08-11 01:05:25 +0800

    add detail page

--- src/views/Detail/template.js
+++ src/views/Detail/template.js
@@ -1,1 +1,30 @@
-export default {}
+import marked from "marked";
+import blog from "@/api/blog.js";
+export default {
+    data() {
+        return {
+            title: '',
+            rawContent: '',
+            user: {
+                avatar: '',
+                username: ''
+            },
+            createdAt: ''
+        }
+    },
+    created() {
+        this.blogId = this.$route.params.blogId;
+        blog.getDetail({ blogId: this.blogId}).then(res => {
+            console.log(res);
+            this.title = res.data.title;
+            this.rawContent = res.data.content;
+            this.user = res.data.user;
+            this.createdAt = res.data.createdAt;
+        })
+    },
+    computed: {
+        markDown() {
+            return marked(this.rawContent)
+        }
+    }
+}
