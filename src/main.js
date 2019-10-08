9ebd269b93dfe56a3f77573d7f47095c86424c32
suyuan <suyuan326@qq.com>
2019-10-08 20:58:59 +0800

    formate

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import store from "./store";
 import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import Util from "./helpers/util";
+// 存储服务
+var AV = require('leancloud-storage');
+var { Realtime } = require('leancloud-realtime'); // 若不使用即时通讯服务，则无需引入
+
+var APP_ID = 'jpOlzgnU9RtM6e5Yfe8wJG5Q-gzGzoHsz';
+var APP_KEY = 'TmHsTJ2nP3ibdx1biKNU2MQh';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
 
 Vue.use(Element);
 Vue.use(Util);
--- src/views/Index/template.js
+++ src/views/Index/template.js
@@ -1,33 +1,40 @@
-import blog from "@/api/blog.js";
+import blog from '@/api/blog.js';
+import AV from 'leancloud-storage';
 
 window.blog = blog;
 
 export default {
-    data() {
-        return {
-            blogs: [],
-            total: 0,
-            page: 1
-        }
-    },
-    created() {   //生命周期函数，模板还没有渲染，数据已经完成时
-        this.page = parseInt(this.$route.query.page) || 1;
-        blog.getIndexBlogs({ page: this.page }).then(res => {
-            console.log(res)
-            this.blogs = res.data;
-            this.total = res.total;
-            this.page = res.page;
-        })
-    },
-    methods: {
-        onPageChange(newPage) {
-            console.log(newPage)
-            blog.getIndexBlogs({ page: newPage }).then(res => {
-                this.blogs = res.data;
-                this.total = res.total;
-                this.page = res.page;
-                this.$router.push({path: '/', query: {page: newPage}})
-            })
-        }
+  data() {
+    return {
+      blogs: [],
+      total: 0,
+      page: 1
+    };
+  },
+  created() {   //生命周期函数，模板还没有渲染，数据已经完成时
+    this.page = parseInt(this.$route.query.page) || 1;
+    var TestObject = AV.Object.extend('TestObject');
+    var testObject = new TestObject();
+    testObject.set('words', 'Hello world!');
+    testObject.save().then(function (testObject) {
+      alert('保存成功。');
+    });
+    blog.getIndexBlogs({ page: this.page }).then(res => {
+      console.log(res);
+      this.blogs = res.data;
+      this.total = res.total;
+      this.page = res.page;
+    });
+  },
+  methods: {
+    onPageChange(newPage) {
+      console.log(newPage);
+      blog.getIndexBlogs({ page: newPage }).then(res => {
+        this.blogs = res.data;
+        this.total = res.total;
+        this.page = res.page;
+        this.$router.push({ path: '/', query: { page: newPage } });
+      });
     }
-}
+  }
+};
