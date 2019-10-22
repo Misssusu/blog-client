9fb763eaa858fc85fbc2bda38f2c1d9bf26fbbd6
suyuan <suyuan326@qq.com>
2019-10-22 12:35:02 +0800

    update

--- src/views/Create/template.js
+++ src/views/Create/template.js
@@ -10,14 +10,14 @@ export default {
       atIndex: ""
     };
   },
-    methods: {
-        onCreate() {
-            if(this.title === '' || this.description === '' || this.content === ''){
-                Message.error("内容不能为空");
-            }else {
+  methods: {
+    onCreate() {
+      if (this.title === "" || this.description === "" || this.content === "") {
+        Message.error("内容不能为空");
+      } else {
                 blog.createBlog({ title: this.title, description: this.description, content: this.content, atIndex: this.atIndex})
                     .then(res => {
-                        this.$message.success(res.msg)
+                        this.$message.success(res.msg);
                         this.$router.push({path: `/detail/${res.data.id}`})
                     })
             }
